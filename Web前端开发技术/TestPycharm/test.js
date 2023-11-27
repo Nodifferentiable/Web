f2bc68d58d3243c32d59ffaaf266a93fa2fa601e
Nodifferentiable <2517831637@qq.com>
2023-11-27 17:07:28 +0800

    1
    1

---
+++ Web前端开发技术/TestPycharm/test.js
@@ -0,0 +1,14 @@
+var person = {
+    name: 'Bob',
+    age: 20,
+    tags: ['js', 'web', 'mobile'],
+    city: 'Beijing',
+    hasCar: true,
+    zipcode: null
+};
+person.tags.pop()
+console.log(person.name);       // 输出 Bob
+console.log(person.tags );        // 输出 2
+
+
+
