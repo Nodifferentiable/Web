fcdd64d0d8c3e20b33844bd07d5df54dac329a7e
Nodifferentiable <2517831637@qq.com>
2023-11-27 21:19:38 +0800

    添加注册app表
    注册表路由和超级管理员后台

--- Web前端开发技术/TestPycharm/Django/BookStore/BookStore/urls.py
+++ Web前端开发技术/TestPycharm/Django/BookStore/BookStore/urls.py
@@ -17,5 +17,6 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
+    #后台管理的路由
     path('admin/', admin.site.urls),
 ]
--- Web前端开发技术/TestPycharm/Django/BookStore/index/admin.py
+++ Web前端开发技术/TestPycharm/Django/BookStore/index/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin #Django自动在admin.py文件中导入
+from index.models import Book, Author,UserInfo #这个需要我们自己导入相应的模型类（数据表）
+
+#后台设置注册
+admin.site.register([Book,Author,UserInfo])
